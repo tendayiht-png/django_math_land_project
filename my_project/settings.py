1854817f1a8fc0d31e6aee8802f94e897625ee17
tendayiht-png <tendayiht@gmail.com>
2026-02-25 22:55:17 +0000

    Updated settings.py with new py app.

--- my_project/settings.py
+++ my_project/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y3f7cl)5pdj1l)s%0c)&@lib)(jh@_45rg7wdj*+1p=3&vtaog'
+import os
+if os.path.isfile('env.py'):
+    import env
+SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-y3f7cl)5pdj1l)s%0c)&@lib)(jh@_45rg7wdj*+1p=3&vtaog'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
