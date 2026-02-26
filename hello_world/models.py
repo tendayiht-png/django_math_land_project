dce35dc0c328420a4e9b60e23776987149c7d02b
tendayiht-png <tendayiht@gmail.com>
2026-02-26 17:57:01 +0000

    Updated Requirements and models for better database integrity and compatibility with Heroku deployment.

--- hello_world/models.py
+++ hello_world/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class GameScore(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     score = models.IntegerField(default=0)
     incorrect = models.IntegerField(default=0)
     game_type = models.CharField(max_length=20)
